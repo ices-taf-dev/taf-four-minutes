12f50c81fc41bd8fdd905df438d86ec4a9a5f6b4
Arni Magnusson <thisisarni@gmail.com>
2025-03-19 08:29:29 +1100

    Save ggplot

--- report.R
+++ report.R
@@ -13,8 +13,9 @@ new_york <- read_csv("data/new_york.csv", col_types=cols())
 coefs <- read_csv("output/coefs.csv", col_types=cols())
 
 taf.png("fit")
-ggplot(new_york) +
+p <- ggplot(new_york) +
   geom_point(aes(x=Temp, y=Ozone)) +
   geom_abline(intercept=coefs[[1]], slope=coefs[[2]]) +
   theme_gray(24)
+plot(p)
 dev.off()
